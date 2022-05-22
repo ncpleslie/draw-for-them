16b53deb911cebc3c228eaebf4831259ee8ad5f1
Nick Leslie <nleslie@jadeworld.com>
2022-05-22 18:14:21 +1200

    changed to ts

---
+++ client/public/main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require("electron");
+const isDev = require("electron-is-dev");
+const path = require("path");
+const { autoUpdater } = require("electron-updater");
+
+const createWindow = () => {
+  const window = new BrowserWindow({ width: 800, height: 480 });
+  autoUpdater.checkForUpdatesAndNotify();
+
+  window.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "./index.html")}`
+  );
+};
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+autoUpdater.on("checking-for-update", () => {
+  console.log("Checking for updates");
+});
+
+autoUpdater.on("update-available", (info) => {
+  console.log("Theres an update", info);
+});
