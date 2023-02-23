3928c4172fd8eb03e632e8103c488c237d01a4d8
Nick Leslie <ncpleslie@gmail.com>
2023-02-23 14:59:15 +1300

    Update AuthAppShell.tsx

--- src/layout/AuthAppShell.tsx
+++ src/layout/AuthAppShell.tsx
@@ -4,7 +4,7 @@ import Header from "../components/header/Header";
 const AuthAppShell: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <>
-      <main className="app-container h-screen overflow-hidden text-icon-inactive">
+      <main className="app-container h-screen h-[100dvh] overflow-hidden overflow-y-hidden text-icon-inactive">
         <Header />
         {children}
       </main>
